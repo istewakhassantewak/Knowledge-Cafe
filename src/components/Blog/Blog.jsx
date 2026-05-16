b7f6dba1d965910e3474f7c05855af3b5bd6ee50
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-16 23:52:37 +0600

    Showing the blog details in ui

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,14 +1,32 @@
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
 
-    console.log(blog)
+    const { id, cover_img, author, author_img, title, posted_date, reading_time, hashtags } = blog
     return (
         <div>
-            
+            <img src={cover_img} alt={`Cover picture of the ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, index) => <span key={index}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
-Blog.propTypes= {
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
 }
 
