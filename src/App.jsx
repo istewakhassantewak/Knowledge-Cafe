b8f7a2ee1783ca7fb07f1b6f6a4d0a0c4fcd8af5
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-16 23:27:57 +0600

    designing header blogs bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
-import Blogs from './components/Header/Blogs/Blogs'
+import Blogs from './components/Blogs/Blogs'
 import Header from './components/Header/Header'
-
+import Bookmarks from './components/Bookmarks/Bookmarks'
 function App() {
 
 
   return (
     <>
       <Header></Header>
+     <div className='md:flex'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+     </div>
+      
 
     </>
   )
